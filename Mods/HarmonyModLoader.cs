9a47d66a408f14ca55ed584016a338c46430b508
P3k <paul.l.b@hotmail.co.uk>
2025-02-18 17:03:14 +0000

    Automated commit on 18-02-2025_17

---
+++ Mods/HarmonyModLoader.cs
@@ -0,0 +1,15 @@
+﻿namespace P3ks_Craftable_Compass.Mods
+{
+   using HarmonyLib;
+
+   using System.Linq;
+
+   public class HarmonyModLoader : IModApi
+   {
+      public void InitMod(Mod _modInstance)
+      {
+         var patch = new Harmony("P3ks Dynamic Music");
+         patch.PatchAll();
+      }
+   }
+}
---
+++ Patches/DynamicMusicPatches.cs
@@ -0,0 +1,103 @@
+﻿namespace P3ks_DynamicMusic.Patches
+{
+   using DynamicMusic;
+
+   using HarmonyLib;
+
+   using System.Linq;
+
+   using UnityEngine;
+
+   [HarmonyPatch(typeof(LayeredSection<FixedLayerMixer>), "FillStream")]
+   public static class FixedLayerMixerPatch
+   {
+      [HarmonyPrefix]
+      public static bool FillStreamPrefix(LayeredSection<FixedLayerMixer> __instance, float[] data)
+      {
+         if (data == null)
+         {
+            Debug.LogWarning("FillStream: data is null");
+            return false;
+         }
+
+         using (LayeredSection<FixedLayerMixer>.s_FillStreamMarker.Auto()) // Use static access
+         {
+            for (var i = 0; i < data.Length; i++)
+            {
+               if (i >= data.Length)
+               {
+                  Debug.LogWarning("FillStream: data out of bounds.");
+                  break;
+               }
+
+               data[i] = __instance.Mixer[__instance.cursor++];
+               // __instance.cursor %= Content.SamplesFor[__instance.Sect] * 2;
+            }
+         }
+
+         return false; // Prevent original method execution
+      }
+   }
+
+   [HarmonyPatch(typeof(LayeredSection<BloodmoonLayerMixer>), "FillStream")]
+   public static class BloodmoonLayerMixerPatch
+   {
+      [HarmonyPrefix]
+      public static bool FillStreamPrefix(LayeredSection<BloodmoonLayerMixer> __instance, float[] data)
+      {
+         if (data == null)
+         {
+            Debug.LogWarning("FillStream: data is null");
+            return false;
+         }
+
+         using (LayeredSection<BloodmoonLayerMixer>.s_FillStreamMarker.Auto()) // Use static access
+         {
+            for (var i = 0; i < data.Length; i++)
+            {
+               if (i >= data.Length)
+               {
+                  Debug.LogWarning("FillStream: data out of bounds.");
+                  break;
+               }
+
+               data[i] = __instance.Mixer[__instance.cursor++];
+               // __instance.cursor %= Content.SamplesFor[__instance.Sect] * 2;
+            }
+         }
+
+         return false; // Prevent original method execution
+      }
+   }
+
+   [HarmonyPatch(typeof(LayeredSection<CombatLayerMixer>), "FillStream")]
+   public static class CombatLayerMixerPatch
+   {
+      [HarmonyPrefix]
+      public static bool FillStreamPrefix(LayeredSection<CombatLayerMixer> __instance, float[] data)
+      {
+         if (data == null)
+         {
+            Debug.LogWarning("FillStream: data is null");
+            return false;
+         }
+
+         using (LayeredSection<CombatLayerMixer>.s_FillStreamMarker.Auto()) // Use static access
+         {
+            for (var i = 0; i < data.Length; i++)
+            {
+               if (i >= data.Length)
+               {
+                  Debug.LogWarning("FillStream: data out of bounds.");
+                  break;
+               }
+
+               data[i] = __instance.Mixer[__instance.cursor++];
+               // __instance.cursor %= Content.SamplesFor[__instance.Sect] * 2;
+            }
+         }
+
+         return false; // Prevent original method execution
+      }
+   }
+}
